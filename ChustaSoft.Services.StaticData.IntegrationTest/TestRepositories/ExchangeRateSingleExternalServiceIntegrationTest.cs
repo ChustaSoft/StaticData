16cfa0261ba39fa553106e2767e89fee2b6df538
Xavi Rueda Abelaira <xavi_rueda@outlook.com>
2019-01-29 19:41:33 +0100

    Adjusted Package name

--- ChustaSoft.Services.StaticData.IntegrationTest/TestRepositories/ExchangeRateSingleExternalServiceIntegrationTest.cs
+++ ChustaSoft.Services.StaticData.IntegrationTest/TestRepositories/ExchangeRateSingleExternalServiceIntegrationTest.cs
@@ -94,8 +94,8 @@ namespace ChustaSoft.Services.StaticData.IntegrationTest.TestRepositories
         public void Given_CurrenciesAndDateRange_When_GetHistoricalInvoked_Then_ExchangeRatesRetrived()
         {
             string currencyFrom = "USD", currencyTo = "EUR";
-            DateTime beginDate = new DateTime(2018, 1, 1), endDate = new DateTime(2018, 1, 7);
-
+            DateTime beginDate = DateTime.Now.AddDays(-30), endDate = DateTime.Now.AddDays(-25);
+            
             var data = _serviceUnderTest.GetHistorical(currencyFrom, currencyTo, beginDate, endDate).Result;
 
             Assert.IsNotNull(data);
